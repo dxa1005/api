b6ca3c5e92563b9b55b9f3dcb9c5077f9d124b40
Rajkumar2710 <48489181+Rajkumar2710@users.noreply.github.com>
2022-07-12 12:25:18 +0530

    Blob code in Repo

--- API/UserDataAPI/UserDataAPI/Repo/UserRepo.cs
+++ API/UserDataAPI/UserDataAPI/Repo/UserRepo.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +12,10 @@ using Dapper;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Core;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
-//using System.Web.Script.Serialization;
 
 namespace UserDataAPI.Repo
 {
@@ -94,6 +96,38 @@ namespace UserDataAPI.Repo
             {
                 return secretValue = ex.ToString();
             }
+        }
+        
+         public async Task<string> GetBlobs()
+        {
+            string res = null;
+            try
+            {
+                
+                string blobContainter = Configuration["Blob:Container"]; 
+                string accountName = Configuration["Blob:StorageAccount"];
+                int count = 0;
+                string containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}",
+                              accountName, blobContainter);
+                BlobContainerClient containerClient
+                      = new BlobContainerClient(new Uri(containerEndpoint),
+                                                new DefaultAzureCredential());
+                //BlobContainerClient containerClient = new BlobContainerClient(blobConString, blobContainter);
+                string blobNames = null;
+                await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+                {
+                    blobNames = blobNames+blobItem.Name+", ";
+                    count++;
+                }
+                string response = null;
+                response = "No. of blobs: " + count.ToString() + (char)10 + "Blob Names: " + blobNames ;
+                res = response.ToString();                
+            }
+            catch (Exception ex)
+            {
+                res = ex.ToString();
+            }
+            return res;
         }
     }
 
